b06070918622436ac10387cb8c7e782ec78ed23b
crisbux <cristiandab@gmail.com>
2024-03-28 17:47:05 -0700

    Verse
    Animation and change between different verses

--- docs/app.js
+++ docs/app.js
@@ -2,7 +2,7 @@
 
 // Cristian Betancourt
 
-// Carousel
+// CAROUSEL
 
 // Variable Hero
 const heroContainer = document.querySelector(".hero");
@@ -41,4 +41,49 @@ changeImage();
 function autoChangeImage() {
     showNextImage();
 }
-setInterval(autoChangeImage, 4000);
+setInterval(autoChangeImage, 5000);
+
+
+// VERSES
+
+const verses = [
+    {
+        text: "Then they who are alive, shall be caught up together into the clouds with them who remain, to meet the Lord in the air; and so shall we be ever with the Lord.",
+        reference: "1 Thessalonians 4:17"
+    },
+    {
+        text: "For God so loved the world, that he gave his only Son, \n that whoever believes in him should not perish but have eternal life.",
+        reference: "John 3:16"
+    },
+    {
+        text: "Have I not commanded you? Be strong and courageous. Do not be afraid; do not be discouraged, for the Lord your God will be with you wherever you go.",
+        reference: "Joshua 1:9"
+    }
+];
+
+let currentVerseIndex = 0;
+const verseElement = document.getElementById('verse');
+const referenceElement = document.getElementById('reference');
+
+function showVerse(index) {
+    verseElement.style.opacity = 0;
+    setTimeout(() => {
+        verseElement.textContent = verses[index].text;
+        referenceElement.textContent = verses[index].reference;
+        verseElement.style.opacity = 1;
+    }, 1000);
+}
+
+function showNextVerse() {
+    currentVerseIndex = (currentVerseIndex + 1) % verses.length;
+    showVerse(currentVerseIndex);
+}
+
+function showPreviousVerse() {
+    currentVerseIndex = (currentVerseIndex - 1 + verses.length) % verses.length;
+    showVerse(currentVerseIndex);
+}
+
+// Change verses 
+setInterval(showNextVerse, 5000);
+showVerse(currentVerseIndex);

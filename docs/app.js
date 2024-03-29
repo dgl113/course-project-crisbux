6ae8a1c13e90657b2ecbb6a0a9b19ac14c3110dc
crisbux <cristiandab@gmail.com>
2024-03-28 17:09:23 -0700

    Carousel changes
    Fix Previous button and time
    Review all code and minor corrections

--- docs/app.js
+++ docs/app.js
@@ -1,46 +1,44 @@
-'use strict';
+"use strict";
 
 // Cristian Betancourt
 
-// CAROUSEL
+// Carousel
 
 // Variable Hero
-const heroContainer = document.querySelector('.hero');
-const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn');
+const heroContainer = document.querySelector(".hero");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
 
 // Array URL images
-const images = [
-    'image1.jpg',
-    'image2.jpg',
-    'image3.jpg'
-];
+const images = ["image1.jpg", "image2.jpg", "image3.jpg"];
 
-let currentImageIndex = 0; 
+let currentImageIndex = 0;
 
 // Function Carousel
 
 function changeImage() {
-    // Background change
-    heroContainer.style.backgroundImage = `url(images/${images[currentImageIndex]})`;
-    currentImageIndex++;
-
-    function showPreviousImage() {
-        currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-        changeImage(currentImageIndex);
-    }
-
-    function showNextImage() {
-        currentImageIndex = (currentImageIndex + 1) % images.length;
-        changeImage(currentImageIndex);
-    }
-
-    prevBtn.addEventListener('click', showPreviousImage);
-    nextBtn.addEventListener('click', showNextImage);
+  // Background change
+  heroContainer.style.backgroundImage = `url(images/${images[currentImageIndex]})`;
 }
+
+function showPreviousImage() {
+  currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+  changeImage();
+}
+
+function showNextImage() {
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  changeImage();
+}
+
+prevBtn.addEventListener("click", showPreviousImage);
+nextBtn.addEventListener("click", showNextImage);
 
 // Call funtion
 changeImage();
 
 // Interval changer
-setInterval(changeImage, 5000);
+function autoChangeImage() {
+    showNextImage();
+}
+setInterval(autoChangeImage, 4000);

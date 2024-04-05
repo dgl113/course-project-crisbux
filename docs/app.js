9a9ed5bbe8fce4dfa991ba187794abc7aa16db49
crisbux <cristiandab@gmail.com>
2024-04-05 12:43:00 -0700

    Form Validation
    first steps

--- docs/app.js
+++ docs/app.js
@@ -90,3 +90,11 @@ showVerse(currentVerseIndex);
 
 
 //FORM VALIDATION
+
+const form = document.getElementById('form');
+const inputs = document.querySelectorAll('#form input');
+
+form.addEventListener('submit', (e) => {
+e.preventDefault();
+
+});

c6ce79ae794cc0cd47df62aa6a641f0b631e584f
crisbux <cristiandab@gmail.com>
2024-04-05 19:41:38 -0700

    reset form

--- docs/form.js
+++ docs/form.js
@@ -49,6 +49,7 @@ document.addEventListener('DOMContentLoaded', function () {
         } else {
             // If no errors, show success prompt
             alert('Submission successful!');
+            form.reset();
             event.preventDefault();
         }
     });
